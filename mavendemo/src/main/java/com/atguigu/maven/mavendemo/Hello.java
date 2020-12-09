ba53b3d601820593659bafd4420799c57ae29a73
leiwuheng <1873504905@qq.com>
2020-12-09 21:31:32 +0800

    添加文件

---
+++ mavendemo/src/main/java/com/atguigu/maven/mavendemo/Hello.java
@@ -0,0 +1,11 @@
+package com.atguigu.maven.mavendemo;
+
+/**
+ * @author
+ * @create 2020-12-09 17:30
+ */
+public class Hello {
+    public String sayHello(String name){
+        return "Hello" + name + "!";
+    }
+}
---
+++ mavendemo/src/test/java/com/atguigu/maven/mavendemo/MavendemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atguigu.maven.mavendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavendemoApplicationTests {
+
+    @Test
+    void testHello() {
+        Hello hello = new Hello();
+        String maven = hello.sayHello("maven");
+        System.out.println(maven);
+    }
+
+}
