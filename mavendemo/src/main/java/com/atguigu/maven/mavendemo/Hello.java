56fcdc86dd82acdeb49148a2ad0e13ef2faa5364
leiwuheng <1873504905@qq.com>
2020-12-09 22:12:41 +0800

    分支添加

--- mavendemo/src/main/java/com/atguigu/maven/mavendemo/Hello.java
+++ mavendemo/src/main/java/com/atguigu/maven/mavendemo/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
     public String sayHello(String name) {
 
         System.out.println("更新1");
-        System.out.println("创建分支");
+        System.out.println("分支添加");
 
         return "Hello" + name + "!";
 
